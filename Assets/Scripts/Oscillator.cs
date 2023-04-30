1df0aea9f255abf52f89b311dd99969a872b040b
Henry Moule <hen10@hotmail.co.uk>
2023-04-30 19:01:24 +0100

    moving obstacles and quick application script

--- Assets/Scripts/Oscillator.cs
+++ Assets/Scripts/Oscillator.cs
@@ -6,7 +6,7 @@ public class Oscillator : MonoBehaviour
 {
     Vector3 startingPosition;
     [SerializeField] Vector3 movementVector;
-    [SerializeField] [Range(0,1)] float movementFactor;
+    float movementFactor;
     [SerializeField] float period = 2f;
     // Start is called before the first frame update
     void Start()
@@ -17,9 +17,13 @@ public class Oscillator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float cycles = Time.time / period;
-        const float tau = Mathf.PI * 2;
-        float rawSineWave = Mathf.Sin(cycles * tau);
+        if (period <= Mathf.Epsilon) return;
+        float cycles = Time.time / period; // continually growing over time
+        const float tau = Mathf.PI * 2; // constant value of 6.283
+        float rawSineWave = Mathf.Sin(cycles * tau); // going from -1 to 1
+
+        movementFactor = (rawSineWave + 1f) / 2F; // recalculated to go from 0 to 1 so its cleaner
+        
         Vector3 offset = movementVector * movementFactor;
         transform.position = startingPosition + offset;
     }
